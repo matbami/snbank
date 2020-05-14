9e4643c6879cfb35a347e56e37c2a7b33050983d
Ayobami <adelekeayobami13@gmail.com>
2020-05-14 19:30:11 +0100

    Another commit

--- program.py
+++ program.py
@@ -1,3 +1,23 @@
 print('Do you want to login')
-closeApp =input(print('Login?'))
-loginApp = input(print('close App?'))
+closeApp =input(print('Login? or close app'))
+
+
+
+if(closeApp == 'login'):
+    username=input(print('enter your username'))
+    password = input(print('enter your password'))
+
+ques = input(print('Do you want to create new account orcheck details or logout'))
+
+    if(ques == 'createaccount'):
+        accname = input(print('enter acc name'))
+        oppbal = input(print('enter opening balance'))
+        acctype = input(print('enter acc type'))
+        accemail = input(print('enter acc email'))
+
+elif(closeApp == 'checkaccount details'):
+      accname = input(print('enter acc name'))
+
+
+elif(closeApp == 'logout'):
+    
